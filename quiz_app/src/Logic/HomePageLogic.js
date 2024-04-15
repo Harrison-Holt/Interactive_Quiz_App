f0ca53e072c277f58bbb9cb3378f0ee945620cf4
Harrison-Holt <hholt2901@gmail.com>
2024-04-15 10:48:06 -0400

    Added a consol.log() to see what the api sent back

--- quiz_app/src/Logic/HomePageLogic.js
+++ quiz_app/src/Logic/HomePageLogic.js
@@ -6,6 +6,8 @@ const categoryMapping = {
     "Science: Computers": 18,
 };
 
+// TODO: Need to figure out why the api is not returning the values 
+// ? 
 export async function fetchQuizQuestions(numQuestions, category, difficulty) {
     const apiCategory = categoryMapping[category];
     const apiDifficulty = difficulty.toLowerCase();
@@ -15,7 +17,7 @@ export async function fetchQuizQuestions(numQuestions, category, difficulty) {
         const response = await fetch(apiUrl);
         if (!response.ok) throw new Error('Response not OK');
         const data = await response.json();
-        console.log(data); 
+        console.log(data.results); 
         return data.results; 
     } catch (error) {
         console.error("Error fetching questions:", error);
