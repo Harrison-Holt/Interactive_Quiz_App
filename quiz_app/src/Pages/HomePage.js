0ddf6f680079539398eeb4120b8446715fbb5c99
Harrison-Holt <hholt2901@gmail.com>
2024-04-15 11:32:48 -0400

    Remove the values in the useState function

--- quiz_app/src/Pages/HomePage.js
+++ quiz_app/src/Pages/HomePage.js
@@ -4,9 +4,9 @@ import { fetchQuizQuestions } from '../Logic/HomePageLogic';
 
 function Homepage() {
 
-    const [num_questions, setNumQuestions] = useState(5);
-    const [difficulty, setDifficulty] = useState('Easy');
-    const [category, setCategory] = useState('general_knownlegde');
+    const [num_questions, setNumQuestions] = useState();
+    const [difficulty, setDifficulty] = useState('');
+    const [category, setCategory] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
