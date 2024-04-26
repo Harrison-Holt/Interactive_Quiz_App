7bef6a91c9c9fe18d585888160efb7bf244c21ff
Harrison-Holt <hholt2901@gmail.com>
2024-04-26 10:14:56 -0400

    improve the css

--- quiz_app/src/Pages/Quiz.js
+++ quiz_app/src/Pages/Quiz.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import './TriviaComponent.css'; // Import your CSS file
+import './quiz.css'; // Import your CSS file
 
 const TriviaComponent = () => {
     const { state } = useLocation();
