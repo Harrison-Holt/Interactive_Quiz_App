ff37e74283c124e32d26f49b01470f074df2dbf3
Harrison-Holt <hholt2901@gmail.com>
2024-04-26 12:22:27 -0400

    improve the css

--- quiz_app/src/UI/Footer.js
+++ quiz_app/src/UI/Footer.js
@@ -16,7 +16,7 @@ const footerStyle = {
     color: '#fff', 
     textAlign: 'center', 
     padding: '10px',
-    position: 'fixed', // Keep the footer at the bottom
+    position: 'sticy', // Keep the footer at the bottom
     left: '0',
     bottom: '0',
     width: '100%', // Full width
