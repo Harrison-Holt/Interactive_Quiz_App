8aeb6cfdcc175d5bcc15e0b73a4f6a33202ac14d
hsholt <hsholt@clemson.edu>
2024-09-16 21:08:13 -0400

    fixed files

--- src/UI/Header.js
+++ src/UI/Header.js
@@ -8,7 +8,7 @@ function Header() {
             <Col md={6} className="d-flex justify-content-start align-items-center p-3">
             <h1 className="ms-2">Interactive Quiz Game</h1>
             </Col>
-            <Col md={6} className="d-flex justity-content-end align-items-center p-3">
+            <Col md={6} className="d-flex justify-content-end align-items-center p-3">
             <a href="https://harrisonholt.dev/" className='btn btn-secondary'>Home Page</a>
             </Col>
         </Row>
