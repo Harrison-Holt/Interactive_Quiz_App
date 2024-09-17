2bc0a6b18e8d8904d8a8b7a034e342f3000fc4a7
hsholt <hsholt@clemson.edu>
2024-09-16 20:57:44 -0400

    fixed files

--- src/UI/Header.js
+++ src/UI/Header.js
@@ -5,8 +5,10 @@ import './header.css';
 function Header() {
     return (
         <Row as="header">
-            <Col className="d-flex flex-row justifty-content-between align-items-center">
+            <Col md={6} className="d-flex flex-row justifty-content-start align-items-center">
             <h1 className="ms-2">Interactive Quiz Game</h1>
+            </Col>
+            <Col md={6} className="d-flex flex-row justifty-content-end align-items-center">
             <a href="https://harrisonholt.dev/" className='btn btn-secondary'>Home Page</a>
             </Col>
         </Row>
